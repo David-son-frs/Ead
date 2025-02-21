5a633da88fde8a2c87c63b91dc8ec1b1841b668e
David-son-frs <fdavidson991@gmail.com>
2025-02-21 19:46:41 -0300

    Add files via upload

---
+++ Atv_1.java
@@ -0,0 +1,21 @@
+package Listas_Matrizes_For_Each;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Atv_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] number1, number2, number3, number4, number5;
+
+        for (int number = 0; number < 5; number++) {
+            System.out.println("Digite o numero desejado" + number);
+            number = sc.nextInt();
+
+            System.out.println("Os números que foram digitados são: "+ number + ", " + number + ", " + number + ", " + number+ ", " + number);
+
+            }
+
+        }
+    }
+
+
