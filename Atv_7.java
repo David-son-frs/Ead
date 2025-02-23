47e791bab4f916932da3365cdc12d9209d23318a
David-son-frs <fdavidson991@gmail.com>
2025-02-23 00:10:30 -0300

    Add files via upload

---
+++ Atv_7.java
@@ -0,0 +1,26 @@
+package Listas_Matrizes_For_Each;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Atv_7 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            ArrayList<Integer> lista = new ArrayList<>();
+
+            for (int i = 0; i < 8; i++) {
+                System.out.print("Digite o " + (i+1) + "º número: ");
+                int numero = sc.nextInt();
+                lista.add(numero);
+            }
+
+
+            System.out.println("Números em ordem crescente:");
+            for (int num : lista) {
+                System.out.println(num);
+            }
+
+            sc.close();
+        }
+    }
+
+
